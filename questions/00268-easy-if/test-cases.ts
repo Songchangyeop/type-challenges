fd2b08969cc7d11dfde13ef6225fc406c8621166
Changyeop Song <waitwait0301@naver.com>
2024-10-18 14:59:31 +0900

    solution: 00268-easy-if

--- questions/00268-easy-if/test-cases.ts
+++ questions/00268-easy-if/test-cases.ts
@@ -1,5 +1,8 @@
 import type { Equal, Expect } from '@type-challenges/utils'
 
+// [SOLUTION]
+type If<C extends boolean, T, F> = C extends true ? T : F
+
 type cases = [
   Expect<Equal<If<true, 'a', 'b'>, 'a'>>,
   Expect<Equal<If<false, 'a', 2>, 2>>,
