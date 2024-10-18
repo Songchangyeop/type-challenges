7fefe9fe5767c57508f7226b363a8dea43e73527
Changyeop Song <waitwait0301@naver.com>
2024-10-18 11:15:01 +0900

    solution: 00013-warm-hello-world

--- questions/00013-warm-hello-world/test-cases.ts
+++ questions/00013-warm-hello-world/test-cases.ts
@@ -1,5 +1,8 @@
 import type { Equal, Expect, NotAny } from '@type-challenges/utils'
 
+// [SOLUTION]
+type HelloWorld = string
+
 type cases = [
   Expect<NotAny<HelloWorld>>,
   Expect<Equal<HelloWorld, string>>,
