4df19867255bc4101eeab140e0369debbc372342
Changyeop Song <waitwait0301@naver.com>
2024-10-18 16:21:15 +0900

    solution: 03312-easy-parameters

--- questions/03312-easy-parameters/test-cases.ts
+++ questions/03312-easy-parameters/test-cases.ts
@@ -1,5 +1,7 @@
 import type { Equal, Expect } from '@type-challenges/utils'
 
+type MyParameters<T extends (...args: any[]) => any> = T extends (...args: infer U) => any ? U : never
+
 function foo(arg1: string, arg2: number): void {}
 function bar(arg1: boolean, arg2: { a: 'A' }): void {}
 function baz(): void {}
